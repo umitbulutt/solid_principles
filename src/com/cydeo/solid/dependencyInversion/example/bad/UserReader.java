75b1d208052592871215ac52cee1d4f8810434cc
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-08-30 21:49:36 +0200

    dependency inversion principle good example

--- src/com/cydeo/solid/dependencyInversion/example/bad/UserReader.java
+++ src/com/cydeo/solid/dependencyInversion/example/bad/UserReader.java
@@ -2,14 +2,19 @@ package com.cydeo.solid.dependencyInversion.example.bad;
 
 public class UserReader {
 
-    private XMLReader xmlReader;            // What happens if we decide to use JSON format instead of XML format?
+    private XMLReader xmlReader;  // What happens if we decide to use JSON format instead of XML format?
+//    private JSONReader jsonReader;
 
-    public UserReader(XMLReader xmlReader) {
+    public UserReader(XMLReader xmlReader) {    // We need to add jsonReader in Constructor too
         this.xmlReader = xmlReader;
     }
 
-    public String getUsername() {
+    public String getUsername() {            // public String getUsernameFromXML()
         return xmlReader.getUsername();
     }
+
+//    public String getUsernameFromJSON() {
+//        return jsonReader.getUsername();
+//    }
 
 }
