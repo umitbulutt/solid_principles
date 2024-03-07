134c8efe2060d966ea18a33e5365a5b03288cfcd
Umit Bulut <umitbulut1993@hotmail.com>
2024-03-07 06:42:34 -0500

    Solid Principles = 03/07/24 - 06:37 AM

--- src/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
+++ src/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
@@ -9,6 +9,7 @@ public class CRUDService {
     public static void read(){
         // Read a user
         // Send user to database related part
+
     }
     public static void update(){
         // Update a user
